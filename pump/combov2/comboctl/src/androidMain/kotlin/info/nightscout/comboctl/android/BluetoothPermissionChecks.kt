299de53e55883ef99fa863e8d43de14ca4ccea0f
2223333333 <104428098+2223333333@users.noreply.github.com>
2026-03-01 02:29:15 +0200

    Update BluetoothPermissionChecks.kt

--- pump/combov2/comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/BluetoothPermissionChecks.kt
+++ pump/combov2/comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/BluetoothPermissionChecks.kt
@@ -2,6 +2,7 @@ package info.nightscout.comboctl.android
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 import info.nightscout.comboctl.base.BluetoothPermissionException
 
@@ -15,7 +16,7 @@ internal fun <T> checkForConnectPermission(androidContext: Context, block: () ->
     checkForPermissions(androidContext, listOf(bluetoothConnectPermission), block)
 
 internal fun <T> checkForPermissions(androidContext: Context, permissions: List<String>, block: () -> T): T {
-    return run {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         val missingPermissions = permissions
             .filter {
                 ContextCompat.checkSelfPermission(androidContext, it) != PackageManager.PERMISSION_GRANTED
@@ -24,13 +25,21 @@ internal fun <T> checkForPermissions(androidContext: Context, permissions: List<
             block.invoke()
         else
             throw AndroidBluetoothPermissionException(missingPermissions)
-    }
+    } else
+        block.invoke()
 }
 
 internal fun runIfScanPermissionGranted(androidContext: Context, block: () -> Unit): Boolean {
-    return if (ContextCompat.checkSelfPermission(androidContext, bluetoothScanPermission) == PackageManager.PERMISSION_GRANTED) {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (ContextCompat.checkSelfPermission(androidContext, bluetoothScanPermission)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            block.invoke()
+            true
+        } else
+            false
+    } else {
         block.invoke()
         true
-    } else
-        false
+    }
 }
